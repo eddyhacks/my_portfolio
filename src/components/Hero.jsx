50b122b47aa041836909f1c1fae9c819130383a9
Eddyhacks <aditya@gmail.com>
2024-09-06 16:38:16 +0530

    third_commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,50 +3,6 @@ import { HERO_CONTENT_1 } from "../constants";
 import { HERO_CONTENT_2 } from "../constants";
 import { HERO_CONTENT_3 } from "../constants";
 import { HERO_CONTENT_4 } from "../constants";
-
-import IconCloud from "@/components/magicui/icon-cloud";
-
-const slugs = [
-  "typescript",
-  "javascript",
-  "dart",
-  "java",
-  "react",
-  "flutter",
-  "android",
-  "html5",
-  "css3",
-  "nodedotjs",
-  "express",
-  "nextdotjs",
-  "prisma",
-  "amazonaws",
-  "postgresql",
-  "firebase",
-  "nginx",
-  "vercel",
-  "testinglibrary",
-  "jest",
-  "cypress",
-  "docker",
-  "git",
-  "jira",
-  "github",
-  "gitlab",
-  "visualstudiocode",
-  "androidstudio",
-  "sonarqube",
-  "figma",
-];
-
-export function IconCloudDemo() {
-  return (
-    <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8 ">
-      <IconCloud iconSlugs={slugs} />
-    </div>
-  );
-}
-
 import profilepic from "../assets/Aditya_goyal_Profile_photo.png";
 import { motion } from "framer-motion";
 
